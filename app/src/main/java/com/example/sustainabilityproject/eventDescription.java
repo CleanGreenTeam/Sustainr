536fa60971b9a4199757aa62e148e69a9d00970a
Chloe Hebert <chhebert@calpoly.edu>
2019-01-19 11:59:30 -0800

    did event description .xml

---
+++ app/src/main/java/com/example/sustainabilityproject/eventDescription.java
@@ -0,0 +1,13 @@
+package com.example.sustainabilityproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class eventDescription extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_description);
+    }
+}
